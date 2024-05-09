10a2bbee98a862e4c42863a54dfe9b8e66a63076
Kit Plummer <kit@ditto.live>
2024-05-08 22:13:53 -0400

    circle pattern

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,50 @@ extern crate log;
 use chrono::{DateTime, Duration, Utc};
 use std::env;
 use std::error::Error;
+use std::str::FromStr;
 use std::time::SystemTime;
 use std::{thread, time};
 use tokio::io::{AsyncWriteExt, Interest};
 use tokio::net::TcpStream;
 
 extern crate exitcode;
+
+use std::f64::consts::PI;
+
+struct Circle {
+    radius: f64,
+    center_lat: f64,
+    center_long: f64,
+}
+
+impl Circle {
+    fn new(radius: f64, center_lat: f64, center_long: f64) -> Circle {
+        Circle {
+            radius,
+            center_lat,
+            center_long,
+        }
+    }
+
+    fn calculate_coordinates(&self, angle: f64) -> (f64, f64) {
+        let lat = self.center_lat + (self.radius / 111.32) * angle.cos();
+        let long = self.center_long
+            + (self.radius / (111.32 * self.center_lat.to_radians().cos())) * angle.sin();
+        (lat, long)
+    }
+
+    fn calculate_heading(&self, lat1: f64, long1: f64, lat2: f64, long2: f64) -> f64 {
+        let d_long = (long2 - long1).to_radians();
+        let lat1_rad = lat1.to_radians();
+        let lat2_rad = lat2.to_radians();
+
+        let y = d_long.sin() * lat2_rad.cos();
+        let x = lat1_rad.cos() * lat2_rad.sin() - lat1_rad.sin() * lat2_rad.cos() * d_long.cos();
+
+        let initial_bearing = y.atan2(x).to_degrees();
+        (initial_bearing + 360.0) % 360.0
+    }
+}
 
 fn iso8601_plus(st: &std::time::SystemTime, minutes: i64) -> String {
     let dt: DateTime<Utc> = (*st).into();
@@ -37,14 +75,36 @@ async fn main() -> Result<(), Box<dyn Error>> {
             std::process::exit(exitcode::OK);
         }
         "fake" => {
+            let lat: f64 = if args.len() == 4 {
+                f64::from_str(&args[3]).unwrap()
+            } else {
+                37.0
+            };
+            let lon = if args.len() == 5 {
+                f64::from_str(&args[4]).unwrap()
+            } else {
+                -75.0
+            };
+            let circle = Circle::new(10.0, lat, lon);
+
             loop {
-                let _ready = stream.ready(Interest::WRITABLE).await?;
-                // No need for async here, just write and sleep
-                let st = SystemTime::now();
-                let now = iso8601(&st);
-                let tom = iso8601_plus(&st, 10);
-                let msg = format!(
-                    "<?xml version=\"1.0\" standalone=\"yes\"?>
+                let num_points = 100;
+
+                for i in 0..num_points {
+                    let angle = 2.0 * PI * (i as f64) / (num_points as f64);
+
+                    let (lat, long) = circle.calculate_coordinates(angle);
+                    let (next_lat, next_long) = circle
+                        .calculate_coordinates(2.0 * PI * ((i + 1) as f64) / (num_points as f64));
+                    let heading = circle.calculate_heading(lat, long, next_lat, next_long);
+                    let _ready = stream.ready(Interest::WRITABLE).await?;
+
+                    // No need for async here, just write and sleep
+                    let st = SystemTime::now();
+                    let now = iso8601(&st);
+                    let tom = iso8601_plus(&st, 10);
+                    let msg = format!(
+                        "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>
 <event
   how=\"m-s\"
   stale=\"{tom}\" start=\"{now}\" time=\"{now}\"
@@ -52,22 +112,23 @@ type=\"a-f-S-X\"
 uid=\"{}\"
 version=\"2.0\">
 <detail>
-<track course=\"30.9\" heading=\"287.2\" speed=\"1.36\" />
+<track course=\"30.9\" heading=\"{heading}\" speed=\"1.36\" />
 <status battery=\"59\" health=\"good\" />
 <goal lat=\"37.3264235\" lon=\"-75.29052422\"/>
 <camera hfov=\"120\" rel_az=\"0\"/>
 </detail>
-<point ce=\"5\" hae=\"0.0\" lat=\"37.234234\" le=\"0.5\" lon=\"-75.123233\" />
+<point ce=\"5\" hae=\"0.0\" lat=\"{lat}\" le=\"0.5\" lon=\"{long}\" />
 </event>
 ",
-                    target_tcp_addr
-                );
-                debug!("XML: {}", msg);
-                let result = stream.write_all(msg.as_bytes()).await;
-                info!("XML write: success={:?}", result.is_ok());
+                        target_tcp_addr
+                    );
+                    debug!("XML: {}", msg);
+                    let result = stream.write_all(msg.as_bytes()).await;
+                    info!("XML write: success={:?}", result.is_ok());
 
-                let millis = time::Duration::from_millis(2000);
-                thread::sleep(millis);
+                    let millis = time::Duration::from_millis(2000);
+                    thread::sleep(millis);
+                }
             }
         }
         _ => {
